86f8fffc959ba6b29f5f262c540c77e57fb2da5d
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2023-07-09 21:27:59 +0200

    added some options

--- mappings.lua
+++ mappings.lua
@@ -3,26 +3,81 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
-return {
-  -- first key is the mode
+local astro_utils = require "astronvim.utils"
+local mappings = {
   n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
-      end,
-      desc = "Pick to close",
+    -- disable default bindings
+    ["<C-Down>"] = false,
+    ["<C-Left>"] = false,
+    ["<C-Right>"] = false,
+    ["<C-Up>"] = false,
+    ["<C-q>"] = false,
+    ["<C-s>"] = false,
+    ["q:"] = ":",
+    -- better buffer navigation
+    ["]b"] = false,
+    ["[b"] = false,
+    ["<C-d>"] = {"<C-d>zz"},
+    ["<C-u>"] = {"<C-u>zz"},
+    ["<S-l>"] = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
     },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<S-h>"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
+    -- better increment/decrement
+    ["-"] = { "<c-x>", desc = "Descrement number" },
+    ["+"] = { "<c-a>", desc = "Increment number" },
+    -- resize with arrows
+    ["<Up>"] = { function() require("smart-splits").resize_up(2) end, desc = "Resize split up" },
+    ["<Down>"] = { function() require("smart-splits").resize_down(2) end, desc = "Resize split down" },
+    ["<Left>"] = { function() require("smart-splits").resize_left(2) end, desc = "Resize split left" },
+    ["<Right>"] = { function() require("smart-splits").resize_right(2) end, desc = "Resize split right" },
+    -- buffer switching
+    ["<Tab>"] = {
+      function()
+        if #vim.t.bufs > 1 then
+          require("telescope.builtin").buffers { sort_mru = true, ignore_current_buffer = true }
+        else
+          astro_utils.notify "No other buffers open"
+        end
+      end,
+      desc = "Switch Buffers",
+    },
+    -- telescope plugin mappings
+    ["<leader>fx"] = {
+      function() require("telescope").extensions.live_grep_args.live_grep_args() end,
+      desc = "Find words (args)",
+    },
+    ["<leader>fB"] = { "<cmd>Telescope bibtex<cr>", desc = "Find BibTeX" },
+    ["<leader>fe"] = { "<cmd>Telescope file_browser<cr>", desc = "File explorer" },
+    ["<leader>fp"] = { function() require("telescope").extensions.projects.projects {} end, desc = "Find projects" },
+    ["<leader>fT"] = { "<cmd>TodoTelescope<cr>", desc = "Find TODOs" },
   },
+  v = {
+  },
+  i = {
+    -- signature help, fails silently so attach always
+    ["<C-l>"] = { function() vim.lsp.buf.signature_help() end, desc = "Signature help" },
+    ["<S-Tab>"] = { "<C-V><Tab>", desc = "Tab character" },
+  },
+  -- terminal mappings
   t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
+    ["<C-BS>"] = { "<C-\\><C-n>", desc = "Terminal normal mode" },
+    ["<esc><esc>"] = { "<C-\\><C-n>:q<cr>", desc = "Terminal quit" },
+  },
+  x = {
+    -- better increment/decrement
+    ["+"] = { "g<C-a>", desc = "Increment number" },
+    ["-"] = { "g<C-x>", desc = "Descrement number" },
+  },
+  o = {
+    -- line text-objects
+    ["il"] = { ":normal vil<cr>", desc = "Inside line text object" },
+    ["al"] = { ":normal val<cr>", desc = "Around line text object" },
   },
 }
+
+return mappings
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,12 +1,20 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+  -- opts = function(_, opts)
+  --   -- add more things to the ensure_installed table protecting against community packs modifying it
+  --   opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+  --     "lua",
+  --     "python",
+  --     "julia",
+  --     "markdown"
+  --   })
+  -- end,
+  opts = {
+    ensure_installed = {
       "lua",
       "python",
       "julia",
       "markdown"
-    })
-  end,
+    }
+  }
 }
